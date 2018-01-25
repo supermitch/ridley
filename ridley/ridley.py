fa22bf8621a0ba57fc97357b539e0e8f5b05861c
Mitch LeBlanc <supermitch@gmail.com>
2018-01-24 20:24:02 -0800

    Use INI config file

--- ridley/ridley.py
+++ ridley/ridley.py
@@ -1,31 +1,39 @@
+from configparser import ConfigParser
 import os
 
 import praw
 
 
-def get_reddit(client, secret, user, password):
+def read_config(ini_file):
+    config = ConfigParser()
+    config.read(ini_file)
+    return config
+
+
+def get_reddit(config):
     return praw.Reddit(
-        client_id=client,
-        client_secret=secret,
-        username=user,
-        password=password,
-        user_agent='python:ridley:0.1 (by /u/{})'.format(user),
+        client_id=config['client_id'],
+        client_secret=config['client_secret'],
+        username=config['username'],
+        password=config['password'],
+        user_agent='python:ridley:0.1 (by /u/{})'.format(config['username']),
     )
 
 
 def main():
-    user = os.environ['USER']
-    password = os.environ['PWD']
-    client = os.environ['CLIENT']
-    secret = os.environ['SECRET']
+    config = read_config('local.ini')
 
-    reddit = get_reddit(client, secret, user, password)
+    reddit = get_reddit(config['Auth'])
 
+    user = config['Auth']['username']
     redditor = reddit.redditor(user)
-    print(redditor.link_karma)  # Output: users's karma
+    print('Link karma: {}'.format(redditor.link_karma))
 
-    for sub in reddit.redditor(user).submissions:
+    print(vars(reddit.user.me()))
+
+    for sub in reddit.user.me().comments.top('week'):
         print(sub)
+        print(vars(sub))
 
 
 if __name__ == '__main__':
